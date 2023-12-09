f68d0ade8f4d0a682764a0ec34d9465a1fa57546
hendisantika <hendisantika@yahoo.co.id>
2023-12-09 09:53:58 +0700

    Add parseHttpRangeString in Range 🫸🌀✏️📗🐧🐳

--- src/main/java/com/hendisantika/springbootminio/util/Range.java
+++ src/main/java/com/hendisantika/springbootminio/util/Range.java
@@ -26,4 +26,18 @@ public class Range {
     public long getRangeEnd(long fileSize) {
         return Math.min(end, fileSize - 1);
     }
+
+    public static Range parseHttpRangeString(String httpRangeString, int defaultChunkSize) {
+        if (httpRangeString == null) {
+            return Range.builder().start(0).end(defaultChunkSize).build();
+        }
+        int dashIndex = httpRangeString.indexOf("-");
+        long startRange = Long.parseLong(httpRangeString.substring(6, dashIndex));
+        String endRangeString = httpRangeString.substring(dashIndex + 1);
+        if (endRangeString.isEmpty()) {
+            return Range.builder().start(startRange).end(startRange + defaultChunkSize).build();
+        }
+        long endRange = Long.parseLong(endRangeString);
+        return Range.builder().start(startRange).end(endRange).build();
+    }
 }
