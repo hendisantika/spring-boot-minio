270761122f46d9b66a51823791f3e9d47171f790
hendisantika <hendisantika@yahoo.co.id>
2023-12-09 07:14:20 +0700

    Add starter project🫸🌀✏️📗🐧🐳

---
+++ src/test/java/com/hendisantika/springbootminio/SpringBootMinioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootminio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMinioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
