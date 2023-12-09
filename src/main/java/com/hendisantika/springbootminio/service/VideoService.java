0798d3941638aa9299ef782f03b7579b6f002f21
hendisantika <hendisantika@yahoo.co.id>
2023-12-09 09:59:31 +0700

    Add save in VideoService 🫸🌀✏️📗🐧🐳

--- src/main/java/com/hendisantika/springbootminio/service/VideoService.java
+++ src/main/java/com/hendisantika/springbootminio/service/VideoService.java
@@ -1,9 +1,15 @@
 package com.hendisantika.springbootminio.service;
 
+import com.hendisantika.springbootminio.entity.FileMetadataEntity;
+import com.hendisantika.springbootminio.exception.StorageException;
 import com.hendisantika.springbootminio.repository.FileMetadataRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.UUID;
 
 /**
  * Created by IntelliJ IDEA.
@@ -22,4 +28,22 @@ public class VideoService {
     private final MinioStorageService storageService;
 
     private final FileMetadataRepository fileMetadataRepository;
+
+    @Transactional
+    public UUID save(MultipartFile video) {
+        try {
+            UUID fileUuid = UUID.randomUUID();
+            FileMetadataEntity metadata = FileMetadataEntity.builder()
+                    .id(fileUuid.toString())
+                    .size(video.getSize())
+                    .httpContentType(video.getContentType())
+                    .build();
+            fileMetadataRepository.save(metadata);
+            storageService.save(video, fileUuid);
+            return fileUuid;
+        } catch (Exception ex) {
+            log.error("Exception occurred when trying to save the file", ex);
+            throw new StorageException(ex);
+        }
+    }
 }
