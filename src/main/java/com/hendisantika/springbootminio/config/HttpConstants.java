82315d5954cf58d9fc997d8836be03f1a531d9d6
hendisantika <hendisantika@yahoo.co.id>
2023-12-09 10:01:12 +0700

    Add HttpConstants 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/com/hendisantika/springbootminio/config/HttpConstants.java
@@ -0,0 +1,15 @@
+package com.hendisantika.springbootminio.config;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-minio
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 12/9/23
+ * Time: 10:01
+ * To change this template use File | Settings | File Templates.
+ */
+public class HttpConstants {
+    public static final String ACCEPTS_RANGES_VALUE = "bytes";
+}
