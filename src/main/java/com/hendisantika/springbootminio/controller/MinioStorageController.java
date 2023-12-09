703cd65b711581541c349df479326bceb6b5fcf9
hendisantika <hendisantika@yahoo.co.id>
2023-12-09 10:42:59 +0700

    Bug Fix code 🫸🌀✏️📗🐧🐳

--- src/main/java/com/hendisantika/springbootminio/controller/MinioStorageController.java
+++ src/main/java/com/hendisantika/springbootminio/controller/MinioStorageController.java
@@ -1,18 +1,7 @@
 package com.hendisantika.springbootminio.controller;
 
-import com.hendisantika.springbootminio.service.MinioAdapter;
-import io.minio.messages.Bucket;
 import lombok.RequiredArgsConstructor;
-import org.springframework.core.io.ByteArrayResource;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by IntelliJ IDEA.
@@ -27,32 +16,31 @@ import java.util.Map;
 @RestController
 @RequiredArgsConstructor
 public class MinioStorageController {
-    private final MinioAdapter minioAdapter;
-
-    @GetMapping(path = "/buckets")
-    public List<Bucket> listBuckets() {
-        return minioAdapter.getAllBuckets();
-    }
-
-    @PostMapping(path = "/upload", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
-    public Map<String, String> uploadFile(@RequestPart(value = "file", required = false) MultipartFile files) throws IOException {
-        minioAdapter.uploadFile(files.getOriginalFilename(), files.getBytes());
-        Map<String, String> result = new HashMap<>();
-        result.put("key", files.getOriginalFilename());
-        return result;
-    }
-
-    @GetMapping(path = "/download")
-    public ResponseEntity<ByteArrayResource> downloadFile(@RequestParam(value = "file") String file) throws IOException {
-        byte[] data = minioAdapter.getFile(file);
-        ByteArrayResource resource = new ByteArrayResource(data);
-
-        return ResponseEntity
-                .ok()
-                .contentLength(data.length)
-                .header("Content-type", "application/octet-stream")
-                .header("Content-disposition", "attachment; filename=\"" + file + "\"")
-                .body(resource);
-
-    }
+//    private final MinioAdapter minioAdapter;
+//
+//    @GetMapping(path = "/buckets")
+//    public List<Bucket> listBuckets() {
+//        return minioAdapter.getAllBuckets();
+//    }
+//
+//    @PostMapping(path = "/upload", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
+//    public Map<String, String> uploadFile(@RequestPart(value = "file", required = false) MultipartFile files) throws IOException {
+//        minioAdapter.uploadFile(files.getOriginalFilename(), files.getBytes());
+//        Map<String, String> result = new HashMap<>();
+//        result.put("key", files.getOriginalFilename());
+//        return result;
+//    }
+//
+//    @GetMapping(path = "/download")
+//    public ResponseEntity<ByteArrayResource> downloadFile(@RequestParam(value = "file") String file) throws IOException {
+//        byte[] data = minioAdapter.getFile(file);
+//        ByteArrayResource resource = new ByteArrayResource(data);
+//
+//        return ResponseEntity
+//                .ok()
+//                .contentLength(data.length)
+//                .header("Content-type", "application/octet-stream")
+//                .header("Content-disposition", "attachment; filename=\"" + file + "\"")
+//                .body(resource);
+//    }
 }
