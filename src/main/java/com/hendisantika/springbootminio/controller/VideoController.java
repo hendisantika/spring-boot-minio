f3d5e1241dc05175de60fc712de0d574922aaea9
hendisantika <hendisantika@yahoo.co.id>
2023-12-09 10:03:08 +0700

    Add fetchChunk in VideoController 🫸🌀✏️📗🐧🐳

--- src/main/java/com/hendisantika/springbootminio/controller/VideoController.java
+++ src/main/java/com/hendisantika/springbootminio/controller/VideoController.java
@@ -1,17 +1,21 @@
 package com.hendisantika.springbootminio.controller;
 
+import com.hendisantika.springbootminio.config.HttpConstants;
 import com.hendisantika.springbootminio.service.VideoService;
+import com.hendisantika.springbootminio.util.Range;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.UUID;
+
+import static org.apache.tomcat.util.http.fileupload.FileUploadBase.CONTENT_LENGTH;
+import static org.springframework.http.HttpHeaders.*;
 
 /**
  * Created by IntelliJ IDEA.
@@ -38,5 +42,20 @@ public class VideoController {
     public ResponseEntity<UUID> save(@RequestParam("file") MultipartFile file) {
         UUID fileUuid = videoService.save(file);
         return ResponseEntity.ok(fileUuid);
+    }
+
+    @GetMapping("/{uuid}")
+    public ResponseEntity<byte[]> fetchChunk(
+            @RequestHeader(value = HttpHeaders.RANGE, required = false) String range,
+            @PathVariable UUID uuid
+    ) {
+        Range parsedRange = Range.parseHttpRangeString(range, defaultChunkSize);
+        VideoService.ChunkWithMetadata chunkWithMetadata = videoService.fetchChunk(uuid, parsedRange);
+        return ResponseEntity.status(HttpStatus.PARTIAL_CONTENT)
+                .header(CONTENT_TYPE, chunkWithMetadata.metadata().getHttpContentType())
+                .header(ACCEPT_RANGES, HttpConstants.ACCEPTS_RANGES_VALUE)
+                .header(CONTENT_LENGTH, calculateContentLengthHeader(parsedRange, chunkWithMetadata.metadata().getSize()))
+                .header(CONTENT_RANGE, constructContentRangeHeader(parsedRange, chunkWithMetadata.metadata().getSize()))
+                .body(chunkWithMetadata.chunk());
     }
 }
