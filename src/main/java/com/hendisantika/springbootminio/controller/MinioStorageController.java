e9ae84f53536e0e2bf32d948b131d174d02f9aab
hendisantika <hendisantika@yahoo.co.id>
2023-12-09 07:30:34 +0700

    Add uploadFile in MinioStorageController🫸🌀✏️📗🐧🐳

--- src/main/java/com/hendisantika/springbootminio/controller/MinioStorageController.java
+++ src/main/java/com/hendisantika/springbootminio/controller/MinioStorageController.java
@@ -3,10 +3,17 @@ package com.hendisantika.springbootminio.controller;
 import com.hendisantika.springbootminio.service.MinioAdapter;
 import io.minio.messages.Bucket;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by IntelliJ IDEA.
@@ -26,5 +33,13 @@ public class MinioStorageController {
     @GetMapping(path = "/buckets")
     public List<Bucket> listBuckets() {
         return minioAdapter.getAllBuckets();
+    }
+
+    @PostMapping(path = "/upload", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
+    public Map<String, String> uploadFile(@RequestPart(value = "file", required = false) MultipartFile files) throws IOException {
+        minioAdapter.uploadFile(files.getOriginalFilename(), files.getBytes());
+        Map<String, String> result = new HashMap<>();
+        result.put("key", files.getOriginalFilename());
+        return result;
     }
 }
